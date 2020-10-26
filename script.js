e801fea557d992759dd9aaba0623e339befd58c2
santiruz <santiagoruiz1011@gmail.com>
2020-10-25 23:57:53 -0400

    Updating comment

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cellDivs = document.querySelectorAll('.game-cell');
 const winPercentDiv = document.querySelector('.winPercentage');
 
 
-//game variables
+//all game variables
 let gameIsLive = true;
 let xIsNext = true;
 let win = false;
